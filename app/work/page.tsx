d823dcbbf014ef719811bce80ca269a835288c75
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-24 11:16:01 -0300

    feat(work): :construction: add barebones work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
-
+"use client";
+import {} from "react-icons/rx";
+import Circles from "@/components/Circles";
+import Bulb from "@/components/Bulb";
+import ServiceSlider from "@/components/ServiceSlider";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../utilities/variants";
 const WorkPage = () => {
   return (
-    <div>WorkPage</div>
-  )
-}
+    <div className="h-full bg-primary-500/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8 "
+            >
+              My Services<span className="text-accent-500">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[480px] mx-auto lg>:mx-0"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              maxime voluptas? Minus culpa quod deserunt aperiam reiciendis
+              eveniet asperiores fugit quisquam error vitae, beatae tempore
+              dolore, voluptatibus laboriosam magnam suscipit?
+            </motion.p>
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            {" "}
+            <ServiceSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
+};
 
-export default WorkPage
+export default WorkPage;
