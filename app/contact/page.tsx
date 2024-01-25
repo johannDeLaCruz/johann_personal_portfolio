e147d31049f402795461f3823d5ba4d8dedb970c
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-25 14:16:55 -0300

    feat(contactPage): :construction: add barebones contact us page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import Circles from "@/components/Circles";
+import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utilities/variants";
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <div className="h-full bg-primary-500/30">
+      <div className=" container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+        <div className="flex">
+          <h2 className="h2 text-center mb-12">
+            Let&lsquo;s <span className="text-accent-500">connect!</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
