030d9efde378d18bec430a10d8c26791cd8b706e
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-25 12:14:43 -0300

    feat(testimonials): :sparkles: add testimonials slider

--- components/TestimonialSlider.tsx
+++ components/TestimonialSlider.tsx
@@ -1,8 +1,69 @@
-
-const TestimonialSlider = () => {
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { BsArrowRight } from "react-icons/bs";
+import { Pagination } from "swiper/modules";
+import Image from "next/image";
+const workSlides = {
+  slides: [
+    {
+      images: [
+        {
+          title: "title",
+          path: "/thumb1.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb2.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb3.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb4.jpg",
+        },
+      ],
+    },
+    {
+      images: [
+        {
+          title: "title",
+          path: "/thumb4.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb1.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb2.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb3.jpg",
+        },
+      ],
+    },
+  ],
+};
+const WorkSlider = () => {
   return (
-    <div>TestimonialSlider</div>
-  )
-}
+    <Swiper
+      spaceBetween={10}
+      pagination={{ clickable: true }}
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {testimonialSlider.slides.map((person, index) => (
+        <SwiperSlide key={index}>
+  
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
-export default TestimonialSlider
+export default WorkSlider;
