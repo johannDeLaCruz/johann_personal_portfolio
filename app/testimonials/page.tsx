1d07157b68bc09e33ac4f2dae4b0e72cad85c07a
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-25 14:01:28 -0300

    feat(testimonials): :lipstick: done UI

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import TestimonialSlider from "@/components/TestimonialSlider";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../utilities/variants";
 
 const TestimonialsPage = () => {
   return (
-    <div>TestimonialsPage</div>
-  )
-}
+    <div className="h-full bg-primary-300/30 py-32 text-center">
+      <div className="container mx-auto flex flex-col justify-center">
+        <motion.h2
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="h2 mb-8 xl:mb-0"
+        >
+          What Clients <span className="text-accent-500">say.</span>
+        </motion.h2>
+        <motion.div
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <TestimonialSlider />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
 
-export default TestimonialsPage
+export default TestimonialsPage;
