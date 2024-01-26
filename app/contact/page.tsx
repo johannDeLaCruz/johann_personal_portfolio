87fd275aa9383f6df0a702695aba088dcca972ba
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-26 12:04:39 -0300

    feat(contactPage): :lipstick: done UI

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,10 +6,36 @@ const ContactPage = () => {
   return (
     <div className="h-full bg-primary-500/30">
       <div className=" container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
-        <div className="flex">
-          <h2 className="h2 text-center mb-12">
+        <div className="flex flex-col w-full max-w-[700px] bg-primary-500/10">
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 text-center mb-12"
+          >
             Let&lsquo;s <span className="text-accent-500">connect!</span>
-          </h2>
+          </motion.h2>
+          <motion.form
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+          >
+            <div className="flex gap-x-6 w-full">
+              <input type="text" placeholder="name" className="input" />
+              <input type="text" placeholder="email" className="input" />
+            </div>
+            <input type="text" placeholder="subject" className="input" />
+            <textarea placeholder="message" className="textarea"></textarea>
+            <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent-500 group">
+              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                Let&apos;s talk
+              </span>
+              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-100 absolute text-[22px]" />
+            </button>
+          </motion.form>
         </div>
       </div>
     </div>
