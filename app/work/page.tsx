5ae2e0332c2213637cfc2a2e184e794ab123819c
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-31 19:20:07 -0300

    fix(work): :bug: fix layout on info cards

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../utilities/variants";
 const WorkPage = () => {
   return (
-    <div className="h-full flex items-center xl:pt-40 pt-64">
+    <div className="h-full flex lg:items-center xl:pt-40 pt-64">
       {/* <Circles /> */}
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">
--- components/WorkSlider.tsx
+++ components/WorkSlider.tsx
@@ -5,11 +5,13 @@ import "swiper/css/pagination";
 import { BsArrowRight } from "react-icons/bs";
 import { Pagination } from "swiper/modules";
 import Image from "next/image";
+import Link from "next/link";
 
 type Image = {
   title: string;
   path: string;
   description: string;
+  link: string;
 };
 
 type Slide = {
@@ -29,24 +31,28 @@ const workSlides: WorkSlides = {
           path: "/thumb-1.png",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          link: "https://tech-hub-woad.vercel.app",
         },
         {
           title: "Lummus Productions",
           path: "/thumb-2.png",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          link: "https://lummus-productions.vercel.app",
         },
         {
           title: "title",
           path: "/thumb-3.jpg",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          link: "",
         },
         {
           title: "title",
           path: "/thumb-4.jpg",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          link: "",
         },
       ],
     },
@@ -57,24 +63,28 @@ const workSlides: WorkSlides = {
           path: "/thumb-5.jpg",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          link: "",
         },
         {
           title: "title",
           path: "/thumb-6.jpg",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          link: "",
         },
         {
           title: "title",
           path: "/thumb-7.jpg",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          link: "",
         },
         {
           title: "title",
           path: "/thumb-8.jpg",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          link: "",
         },
       ],
     },
@@ -90,7 +100,7 @@ const WorkSlider = () => {
     >
       {workSlides.slides.map((slide, index) => (
         <SwiperSlide key={index}>
-          <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+          <div className="grid md:grid-cols-2 grid-cols-1 grid-rows-2 gap-4 cursor-pointer">
             {slide.images.map((image, index) => (
               <div
                 key={index}
@@ -99,7 +109,7 @@ const WorkSlider = () => {
                 <div className="flex items-center justify-center relative overflow-hidden group">
                   <Image alt="" src={image.path} width={500} height={300} />
                   <div className="absolute inset-0 bg-primary-200 opacity-0 group-hover:opacity-100 transition-all duration-700">
-                    <div className="absolute top-10 left-5 translate-y-full group-hover:translate-y-10 group-hover:xl:-translate-y-0.5 transition-all duration-300">
+                    <div className="absolute top-8 left-5 translate-y-full group-hover:translate-y-1 group-hover:xl:-translate-y-0.5 transition-all duration-300">
                       <div className="flex flex-col">
                         <h2 className="font-body text-body2 font-bold">
                           PROJECT:
@@ -110,15 +120,17 @@ const WorkSlider = () => {
                         </h2>
                         <p className="text-body1">{image.description}</p>
                       </div>
-                      <div className="flex items-center gap-x-2 text-body2 tracking-[0.2em] mt-4">
-                        <div className="delay-100">LIVE</div>
-                        <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                          PROJECT
+                      <Link href={image.link}>
+                        <div className="flex items-center gap-x-2 text-body2 tracking-[0.2em] mt-4">
+                          <div className="delay-100">LIVE</div>
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            PROJECT
+                          </div>
+                          <div className="text-h4 translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            {<BsArrowRight />}
+                          </div>
                         </div>
-                        <div className="text-h4 translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-                          {<BsArrowRight />}
-                        </div>
-                      </div>
+                      </Link>
                     </div>
                   </div>
                 </div>
