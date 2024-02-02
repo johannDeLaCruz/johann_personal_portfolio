821a054237fb49e563c51c575cc8d0d384a9fe0b
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-02-02 17:16:28 -0300

    fix(testimonials): :lipstick: fix rounded avatar border

--- components/TestimonialSlider.tsx
+++ components/TestimonialSlider.tsx
@@ -6,12 +6,11 @@ import { FaQuoteLeft } from "react-icons/fa";
 import { Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
 
-
 const testimonialData = [
   {
     image: "/t-avt-1.jpg",
     name: "Wolner Júnior",
-    position: "Lummus Productions - CEO 0- Customer",
+    position: "Lummus Productions - CEO - Customer",
     message:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
@@ -45,7 +44,13 @@ const TestimonialSlider = () => {
             <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
               <div className="flex flex-col justify-center items-center">
                 <div className="mb-2 mx-auto">
-                  <Image src={person.image} width={100} height={100} alt="" />
+                  <Image
+                    src={person.image}
+                    width={100}
+                    height={100}
+                    alt="customer-avatar"
+                    className="rounded-full"
+                  />
                 </div>
                 <div className="text-h3">{person.name}</div>
                 <div className="text-[12px] uppercase font-extralight tracking-widest">
